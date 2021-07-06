fd85527b5940ae626a885cbfa5079685ab73d070
yeseniavr <85712816+yeseniavr@users.noreply.github.com>
2021-07-06 08:16:05 -0400

    Add files via upload

---
+++ ActObligBD2/index.php
@@ -0,0 +1,21 @@
+<?php
+$nombre='yesenia Carolina' ;
+$apellido='Velásquez Rosas';
+$edad=39;
+$hobbie='lectura';
+$editor='Visual Studio Code';
+$SO='Windows';
+
+
+echo 'Nombres: ' , $nombre;
+echo '<br>';
+echo 'Apellidos:', $apellido;
+echo '<br>';
+echo 'Edad:' , $edad;
+echo '<br>';
+echo 'Hobbie:' ,$hobbie;
+echo '<br>';
+echo 'Editor de texto favorito:',$editor;
+echo '<br>';
+echo 'Sitema Operativo:' ,$SO;
+?>
